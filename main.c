1a58a0195d6ad83b985f0430e5dd6792484042b0
Liang Chen Yu <yuliangchen24@gmail.com>
2026-03-19 02:23:00 +0000

    added struct room and compiled

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+typedef struct Room {
+    char *description;
+    struct Room *north;
+    struct Room *south;
+    struct Room *east;
+    struct Room *west;
+
+} Room;
+
 int main(void) {
     printf("Zork Text Based Game\n");
     return 0;
